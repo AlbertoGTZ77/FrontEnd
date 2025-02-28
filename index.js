c495762374c1ee59f2f8bf78b526fb03319a0c43
AlbertoGTZ <albgtz77@gmail.com>
2025-02-27 19:15:20 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var numero1=prompt("ingresa el primer dato")
+var numero2=prompt("ingresa el segundo dato")
+var numero3=prompt("ingresa el tercer dato")
+var resultado=Number(numero1)+Number(numero2)+Number(numero3)
+
+console.log(Number(resultado))
